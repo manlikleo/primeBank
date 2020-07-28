f6c733bce1a2f56c3cce9e4ef18fa7ded4a91a59
olaotan <leo@olaotans-MacBook-Pro.local>
2020-07-28 13:18:52 +0100

    First commit

---
+++ Index.js
@@ -0,0 +1,30 @@
+const navSlide = () => {
+
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav_links');
+    const navLinks = document.querySelectorAll('.nav_links li');
+    // toggle Nav
+
+
+
+
+    burger.addEventListener('click',() => {
+    nav.classList.toggle('nav-active');
+
+
+    //animate links
+    navLinks.forEach((link,index) => {
+        if (link.style.animation) {
+            link.style.animation = '';
+        } else{
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 4 }s`;
+        }
+    });
+    //burger animation
+    burger.classList.toggle('.toggle');
+
+    });
+   
+}
+
+navSlide();
